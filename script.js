2b51e69de651d6bb76f0806a705b08fd5a9338f5
BALAN ANTONIO FLORIN <12999441@itisrossi.edu.it>
2025-01-11 08:48:30 +0100

    manca controllo errori

---
+++ script.js
@@ -0,0 +1,52 @@
+let aprimi = [];
+let arr_glob = [];
+
+function calcola(){
+    let inp = document.getElementById("inp").value;
+    let vis = document.getElementById("inp2");
+
+    if(!isNaN(parseInt(inp))){
+        arr_glob.push(parseInt(inp));
+        vis.innerHTML += parseInt(inp) + " ";
+    }
+}
+
+function calcolavero(){
+    aprimi = [];
+    document.getElementById("ris").innerHTML = "Numeri primi: ";
+    primi_fino(max_arr(arr_glob)+1);
+    for(let i = 0; i<arr_glob.length; i++){
+        if(aprimi.indexOf(arr_glob[i])!=-1){
+            document.getElementById("ris").innerHTML += arr_glob[i] + " ";
+        }
+    }
+}
+
+function cancella(){
+    arr_glob = [];
+    document.getElementById("inp2").innerHTML = "";
+}
+
+function primi_fino(fine){
+    aprimi = [];
+    let primo;
+    for(let i = 2; i<fine; i++){
+        primo = true;
+        for(let j = 2; j<i; j++){
+            if(i%j==0){
+                primo = false;
+            }
+        }
+        if(primo){
+            aprimi.push(i);
+        }
+    }
+}
+
+function max_arr(arr){
+    let max = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i]>max) { max = arr[i]; }
+    }
+    return max;
+}
